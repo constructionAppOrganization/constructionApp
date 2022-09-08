85787f16034b392f4fa13adda419df67b051b2e9
IT20206178 <kanishkasarangadev@gmail.com>
2022-09-08 20:42:38 +0530

    All Registered Users Frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import SignUp from "./component/UserManager/SignUp";
 import Login from "./component/UserManager/Login";
+import RegisteredUsers from "./component/UserManager/RegisteredUsers";
 
 function App() {
   return <div className="App"> 
-  {/* <SignUp/> */}
-  <Login/>
+ 
   </div>;
 }
 
